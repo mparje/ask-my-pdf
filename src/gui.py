5fa0118a3865f8a1f4eed62dcda75a380c97cb09
mparje <126131806+mparje@users.noreply.github.com>
2023-04-13 16:45:00 -0600

    Update gui.py

--- src/gui.py
+++ src/gui.py
@@ -1,5 +1,5 @@
 __version__ = "0.4.8.2"
-app_name = "Pregunte a PDF"
+app_name = "Ask my PDF"
 
 
 # BOILERPLATE
@@ -60,15 +60,24 @@ def ui_spacer(n=2, line=False, next_n=0):
 
 def ui_info():
 	st.markdown(f"""
-	# Pregunte a PDF
+	# Ask my PDF
 	version {__version__}
 	
-	Sistema de preguntas y respuestas construido sobre GPT3.
+	Question answering system built on top of GPT3.
 	""")
 	ui_spacer(1)
-	st.write("Hecho por [Maciej Obarski](https://www.linkedin.com/in/mobarski/). Adaptado por Moris Polanco.", unsafe_allow_html=True)
+	st.write("Made by [Maciej Obarski](https://www.linkedin.com/in/mobarski/).", unsafe_allow_html=True)
 	ui_spacer(1)
-	
+	st.markdown("""
+		Thank you for your interest in my application.
+		Please be aware that this is only a Proof of Concept system
+		and may contain bugs or unfinished features.
+		If you like this app you can ❤️ [follow me](https://twitter.com/KerbalFPV)
+		on Twitter for news and updates.
+		""")
+	ui_spacer(1)
+	st.markdown('Source code can be found [here](https://github.com/mobarski/ask-my-pdf).')
+
 def ui_api_key():
 	if ss['community_user']:
 		st.write('## 1. Optional - enter your OpenAI API key')
@@ -84,7 +93,7 @@ def ui_api_key():
 		with t2:
 			st.text_input('OpenAI API key', type='password', key='api_key', on_change=on_api_key_change, label_visibility="collapsed")
 	else:
-		st.write('## 1. Ingrese su clave API de OpenAI')
+		st.write('## 1. Enter your OpenAI API key')
 		st.text_input('OpenAI API key', type='password', key='api_key', on_change=on_api_key_change, label_visibility="collapsed")
 
 def index_pdf_file():
@@ -111,9 +120,9 @@ def debug_index():
 	ss['debug']['index'] = d
 
 def ui_pdf_file():
-	st.write('## 2. Suba o seleccione su PDF')
+	st.write('## 2. Upload or select your PDF file')
 	disabled = not ss.get('user') or (not ss.get('api_key') and not ss.get('community_pct',0))
-	t1,t2 = st.tabs(['SUBA','SELECCIONE'])
+	t1,t2 = st.tabs(['UPLOAD','SELECT'])
 	with t1:
 		st.file_uploader('pdf file', type='pdf', key='pdf_file', disabled=disabled, on_change=index_pdf_file, label_visibility="collapsed")
 		b_save()
@@ -179,7 +188,7 @@ def ui_hyde_prompt():
 	st.text_area('HyDE prompt', prompts.HYDE, key='hyde_prompt')
 
 def ui_question():
-	st.write('## 3. Pregunte'+(f' to {ss["filename"]}' if ss.get('filename') else ''))
+	st.write('## 3. Ask questions'+(f' to {ss["filename"]}' if ss.get('filename') else ''))
 	disabled = False
 	st.text_area('question', key='question', height=100, placeholder='Enter question here', help='', label_visibility="collapsed", disabled=disabled)
 
@@ -297,7 +306,7 @@ def output_add(q,a):
 with st.sidebar:
 	ui_info()
 	ui_spacer(2)
-	with st.expander('Avanzado'):
+	with st.expander('advanced'):
 		ui_show_debug()
 		b_clear()
 		ui_model()
