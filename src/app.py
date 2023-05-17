a9858376facfa427e51bbc054afd67e03eddd3f3
mparje <126131806+mparje@users.noreply.github.com>
2023-05-17 01:45:28 -0600

    Update app.py

--- src/app.py
+++ src/app.py
@@ -47,10 +47,6 @@ def on_api_key_change():
     ss['debug']['storage.class'] = ss['storage'].__class__.__name__
 
 
-ss['community_user'] = os.getenv('COMMUNITY_USER')
-if 'user' not in ss and ss['community_user']:
-    on_api_key_change() # use community key
-
 # COMPONENTS
 
 def ui_spacer(n=2, line=False, next_n=0):
@@ -73,104 +69,125 @@ def ui_info():
     st.markdown('Source code can be found [here](https://github.com/mobarski/ask-my-pdf).')
 
 def ui_api_key():
-    if ss['community_user']:
-        st.write('## 1. Optional - enter your OpenAI API key')
-        t1,t2 = st.tabs(['community version','enter your own API key'])
-        with t1:
-            pct = model.community_tokens_available_pct()
-            st.write(f'Community tokens available: :{"green" if pct else "red"}[{int(pct)}%]')
-            st.progress(pct/100)
-            st.write('Refresh in: ' + model.community_tokens_refresh_in())
-            st.write('You can sign up to OpenAI and/or create your API key [here](https://platform.openai.com/account/api-keys)')
-            ss['community_pct'] = pct
-            ss['community_tokens_refresh_in'] = model.community_tokens_refresh_in()
-            if pct < 5:
-                st.write('Please consider entering your own API key to be able to use the app.')
+    st.write('## 1. Enter your OpenAI API key')
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def index_pdf_file():
+    if ss['pdf_file']:
+        ss['filename'] = ss['pdf_file'].name
+        if ss['filename'] != ss.get('fielname_done'): # UGLY
+            with st.spinner(f'indexing {ss["filename"]}'):
+                index = model.index_file(ss['pdf_file'], ss['filename'], fix_text=ss['fix_text'], frag_size=ss['frag_size'], cache=ss['cache'])
+                ss['index'] = index
+                debug_index()
+                ss['filename_done'] = ss['filename'] # UGLY
+
+def debug_index():
+    index = ss['index']
+    d = {}
+    d['hash'] = index['hash']
+    d['frag_size'] = index['frag_size']
+    d['n_pages'] = len(index['pages'])
+    d['n_texts'] = sum([len(p['texts']) for p in index['pages']])
+    ss['debug']['index'] = d
+
+def debug_query():
+    query = ss['query']
+    d = {}
+    d['query'] = query
+    d['prompt'] = model.get_prompt(query)
+    d['tokens'] = len(model.to_tokens(query))
+    d['tokens_free'] = model.tokens_left()
+    d['tokens_refresh_in'] = model.tokens_refresh_in()
+    ss['debug']['query'] = d
+
+def debug_model():
+    ss['debug']['model'] = model.debug_model()
+
+def debug_token_limit():
+    ss['debug']['token_limit'] = model.debug_token_limit()
+
+def st_buttons_group(buttons):
+    cols = st.beta_columns(len(buttons))
+    for col, button in zip(cols, buttons):
+        col.button(button)
+
+# MAIN ROUTINE
+
+def main_routine():
+    debug_model()
+    #
+    st.title(f'Ask my PDF {__version__}')
+    ui_info()
+    ui_api_key()
+    #
+    if st.button('I want to enter my own API key'):
+        ss['api_key'] = None
+        on_api_key_change()
+        return
+    #
+    st.header("2. Upload your PDF")
+    ss['pdf_file'] = st.file_uploader("Upload a PDF file", type=['pdf'])
+    st.write('or')
+    if st.button('Example PDFs'):
+        ss['pdf_file'] = model.get_pdf_example()
+    #
+    st.header("3. Advanced options")
+    ss['fix_text'] = st.checkbox("Attempt to fix text errors (experimental)", value=True)
+    ss['frag_size'] = st.number_input("Text fragment size", value=3000)
+    #
+    if st.button('Index PDF'):
+        index_pdf_file()
+    #
+    st.header("4. Ask a question")
+    ss['query'] = st.text_input("Your question")
+    if ss['query']:
+        ss['query'] = ss['query'].strip()
+        st.write(f"Query length: {len(ss['query'])}")
+        debug_query()
+        st.write(f'Tokens left: {model.tokens_left()}')
+        st.write(f'Tokens refresh in: {model.tokens_refresh_in()}')
+        #
+        if model.tokens_left() < 5:
+            st.write('Please consider entering your own API key to be able to use the app.')
+            return
+        #
+        if st.button("Ask"):
+            t0 = now()
+            debug_query()
+            #
+            if not ss.get('index'):
+                st.write("Please upload a PDF and index it first.")
                 return
-            if st.button('I want to enter my own API key'):
-                ss['community_user'] = False
-                on_api_key_change()
-                return
-        with t2:
-            api_key = st.text_input("API key")
-            if api_key:
-                ss['api_key'] = api_key
-                on_api_key_change()
-    else:
-        if not ss['api_key']:
-            st.write('## 1. Enter your OpenAI API key')
-            api_key = st.text_input("API key")
-            if api_key:
-                ss['api_key'] = api_key
-                on_api_key_change()
-                ui_spacer(2)
-                st.stop()
-        else:
-            st.write('## 1. API key set')
+            #
+            result = model.ask(ss['query'], ss['index'], cache=ss['cache'])
+            ss['debug']['answer'] = result
+            model.api_feedback(result)
+            #
+            ss['answer'] = result['answer']
+            ss['tokens_left'] = model.tokens_left()
+            ss['tokens_refresh_in'] = model.tokens_refresh_in()
+            #
+            header2.write(f"Answer ({round(now()-t0, 2)}s):")
+            st.write(ss['answer'])
+            st.markdown('---')
+            #
+            if st.button('Copy answer to clipboard'):
+                st.write('Answer copied to clipboard.')
+                st.text_area('', ss['answer'])
+            #
+            debug_token_limit()
+        #
+        if st.button('Hide answer'):
+            del ss['answer']
+            del ss['debug']['answer']
+            del ss['tokens_left']
+            del ss['tokens_refresh_in']
+            header2.empty()
+            header3.empty()
 
-def ui_model_info():
-    st.write('## 2. Select model')
-    model_names = model.get_model_names()
-    model_name = st.selectbox('Model', model_names, index=model_names.index(model.get_model_name()))
-    model.use_model(model_name)
-    ss['debug']['model.name'] = model_name
+def app():
+    on_api_key_change()
+    main_routine()
 
-def ui_storage_info():
-    st.write('## 3. Select storage')
-    storage_names = storage.get_storage_names()
-    storage_name = st.selectbox('Storage', storage_names, index=storage_names.index(ss['storage'].name if 'storage' in ss else ''))
-    if 'storage' not in ss or storage_name != ss['storage'].name:
-        ss['data_dict'] = ss['storage'].get_data_dict()
-        ss['storage'] = storage.get_storage(storage_name, data_dict=ss['data_dict'])
-    ss['debug']['storage.class'] = ss['storage'].__class__.__name__
-    ss['debug']['storage.name'] = ss['storage'].name
-
-def ui_file_upload():
-    st.write('## 4. Upload PDF file')
-    uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
-    if uploaded_file is not None:
-        file_contents = uploaded_file.read()
-        file_name = uploaded_file.name
-        ss['file_contents'] = file_contents
-        ss['file_name'] = file_name
-
-def ui_question_input():
-    st.write('## 5. Enter your question')
-    question = st.text_input("Question")
-    if question:
-        ss['question'] = question
-
-def ui_feedback():
-    if 'answer' in ss:
-        st.write('## 6. Feedback')
-        if 'feedback_text' not in ss:
-            ss['feedback_text'] = ss.get('answer') or ss.get('debug').get('answer') or ''
-        st.write(ss['feedback_text'])
-        ss['feedback_text'] = st.text_input("If the answer is not satisfactory, please provide feedback.")
-
-def ui_submit():
-    if st.button('Submit'):
-        header3.empty()
-        header1.info('Working...')
-        answer = model.get_answer(ss['question'], ss['file_contents'])
-        ss['answer'] = answer
-        header1.success('Done!')
-        header2.write(answer)
-        feedback_text = ss.get('feedback_text') or ss.get('answer') or ss.get('debug').get('answer')
-        if feedback_text:
-            ss['feedback'].save(feedback_text)
-        ss['debug']['answer'] = answer
-
-# MAIN
-
-header1.info('Welcome to Ask my PDF')
-ui_spacer()
-ui_info()
-ui_spacer()
-ui_api_key()
-ui_spacer()
-ui_model_info()
-ui_spacer()
-ui_storage_info()
-ui_spacer()
-ui_file_upload
+app()
