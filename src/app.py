2a7f52d55d5e0bd0976403c5a1dd36a1c0bb853a
mparje <126131806+mparje@users.noreply.github.com>
2023-05-19 15:27:39 -0600

    Update app.py

--- src/app.py
+++ src/app.py
@@ -166,7 +166,8 @@ def ui_hyde_prompt():
 def ui_question():
 	st.write('## Pregunte '+(f' a {ss["filename"]}' if ss.get('filename') else ''))
 	disabled = False
-	st.text_area('question', key='question', height=100
+	st.text_area('question', key='question', height=
+		    
 def ui_hyde_answer():
 	# TODO: enter or generate
 	pass
