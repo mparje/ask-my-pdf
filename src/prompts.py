edfcf0a878c4767c639c522de3c46a6a9c9ac679
mparje <126131806+mparje@users.noreply.github.com>
2023-08-13 12:11:14 -0600

    Update prompts.py

--- src/prompts.py
+++ src/prompts.py
@@ -9,9 +9,13 @@ TASK = {
         "Después de la cita, escriba una explicación paso a paso. "
         "Utilice viñetas."
     ),
-    'Caso': (
-        "Resuelva el caso que se te plantea basándose en el texto que aparece a continuación. "
-        "Incluya al menos una cita textual (marcada con comillas) y un comentario sobre dónde encontrarla en el texto (es decir, número de artículo y número de página). "
+    'Análisis del subjuntivo': (
+        "Tu tarea es identificar las oraciones en las que se utiliza el modo subjuntivo, así como las oraciones en las que se utiliza el indicativo cuando lo normal sería utilizar el subjuntivo." 
+	"Debes señalar el verbo en subjuntivo en cada caso y especificar en qué tiempo se encuentra. Asegúrate de leer detenidamente los textos proporcionados y no cambiar ningún verbo."
+	"También debes tener cuidado de no equivocarte al identificar un verbo en modo subjuntivo cuando en realidad está en modo indicativo. No incluyas en tu lista las oraciones que no contengan verbos en subjuntivo." 
+	"Al final, indica cuántas oraciones se encuentran en modo subjuntivo y cuántas debieron haber utilizado el subjuntivo. Por favor, proporciona una respuesta clara y concisa, indicando cada oración en la que se utilice el subjuntivo y especificando el tiempo verbal."
+        "Asimismo, identifica las oraciones donde se haya utilizado el indicativo en lugar del subjuntivo, señalando qué verbo debió haberse conjugado en subjuntivo."
+        "Por favor, ten en cuenta que debes ser preciso y cuidadoso en tu respuesta, y asegúrate de comprender correctamente los textos proporcionados antes de identificar los verbos conjugados en modo subjuntivo."
     ),
     'Puntos clave': (
         "Lee detenidamente el documento de abajo y extrae los puntos clave en en viñetas. " 
